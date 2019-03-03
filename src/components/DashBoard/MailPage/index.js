2a3e9e5bb3d4fca0accfbe9a6949852f0269ac95
yamnyk <notAunicorN2018>
2019-03-03 20:23:06 +0200

    MailPage | show mail by id in URL

--- src/components/DashBoard/MailPage/index.js
+++ src/components/DashBoard/MailPage/index.js
@@ -1,13 +1,43 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+
+import "./MailPage.scss"
 
 class MailPage extends Component {
 	render() {
 		return (
-			<div>
-			Gogi!
+			<div className="mail-page">
+				<h1 className="mail-page__subject">
+					{this.props.mail.subject}
+				</h1>
+				<h4 className="mail-page__from">
+					{this.props.mail.from}
+				</h4>
+				<p className="mail-page__text">
+					{this.props.mail.msgText}
+				</p>
 			</div>
 		);
 	}
 }
 
-export default MailPage;
+const mapStateToProps = (state, props) => {
+	const mails = {...state.mails.mailList};
+	let resMail = '';
+	
+	for(let folder in mails) {
+		resMail = mails[folder].find((mail) => {
+			return mail.id == props.match.params.id
+		});
+		if(resMail !== undefined) {
+			break;
+		}
+	}
+	
+	
+	return {
+		mail: resMail ? resMail : null,
+	};
+};
+
+export default connect(mapStateToProps)(MailPage);
